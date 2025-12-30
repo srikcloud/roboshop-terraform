06d364ccaf08a606611e7b1a58182fa8eef32a3f
srikanth152113 <srikanth152113@gmail.com>
2025-12-30 21:32:26 +0530

    updated

--- component/main.tf
+++ component/main.tf
@@ -64,7 +64,8 @@ inline = [
     "sudo dnf install -y epel-release",
     "sudo dnf install -y ansible",
     "sudo dnf install python3.12 python3.12-pip -y",
-    "sudo pip3.12 install ansible"
+    "sudo pip3.12 install ansible",
+    "ansible-pull -i localhost, U https://github.com/srikcloud/roboshop-ansible roboshop.yml -e app_name-${var-name} -e env-dev"
     ]
 }
 }
