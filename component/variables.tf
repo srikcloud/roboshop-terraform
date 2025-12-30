5f17e71a4d5fe5936d6a8e7c4f327eaee7d136e6
srikanth152113 <srikanth152113@gmail.com>
2025-12-30 20:18:56 +0530

    updated

---
+++ component/variables.tf
@@ -0,0 +1,6 @@
+variable "name" {}
+variable "location" {}
+variable "rg_name" {}
+variable "storage_image_reference_id" {}
+variable "ip_configuration_subnet_id" {}  
+variable "zone_name" {}
---
+++ provider.tf
@@ -0,0 +1,4 @@
+provider "azurerm" {
+     features {}
+     subscription_id = "f7fcf972-1a3d-456c-a347-914d12f8c308"
+}
