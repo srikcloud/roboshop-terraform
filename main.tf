b0a5debac9112100f8950b367ad05a9bfcf58dbe
srikanth152113 <srikanth152113@gmail.com>
2026-01-10 19:25:04 +0530

    updated

--- main.tf
+++ main.tf
@@ -16,15 +16,16 @@ module "databases" {
   network_security_group_id  = var.network_security_group_id
 }
 
-# module "applications" {
-#   depends_on                 = [ module.databases ]
-#   for_each                   = var.applications
-#   source                     = "modules/vm"
-#   ip_configuration_subnet_id = var.ip_configuration_subnet_id
-#   name                       = each.key
-#   rg_name                    = var.rg_name
-#   storage_image_reference_id = var.storage_image_reference_id
-#   zone_name                  = var.zone_name
-#   network_security_group_id  = var.network_security_group_id
-# }
+module "applications" {
+  depends_on                 = [ module.databases ]
+  for_each                   = var.applications
+  source                     = "./modules/vm"
+  ip_configuration_subnet_id = var.ip_configuration_subnet_id
+  name                       = each.key
+  rg_name                    = module.resource-group[each.value["rgname"]].name
+  rg_location                = module.resource-group[each.value["rgname"]].location
+  storage_image_reference_id = var.storage_image_reference_id
+  zone_name                  = var.zone_name
+  network_security_group_id  = var.network_security_group_id
+}
 
