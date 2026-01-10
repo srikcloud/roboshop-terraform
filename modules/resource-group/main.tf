d406de9e8f34f8928166ab445e54cb448a393cae
srikanth152113 <srikanth152113@gmail.com>
2026-01-10 18:43:07 +0530

    updated

--- modules/resource-group/main.tf
+++ modules/resource-group/main.tf
@@ -1,4 +1,4 @@
 resource "azurerm_resource_group" "main" {
-  name = var.name
+  name     = var.name
   location = var.location
 }
--- modules/resource-group/outputs.tf
+++ modules/resource-group/outputs.tf
@@ -1,10 +1,11 @@
 output "name" {
   value = azurerm_resource_group.main.name
 }
+
 output "id" {
   value = azurerm_resource_group.main.id
 }
+
 output "location" {
   value = azurerm_resource_group.main.location
 }
-
